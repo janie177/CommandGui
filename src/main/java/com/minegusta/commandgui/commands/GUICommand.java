6a2e19deb110349f50cb4562433496aaf6519eac
Jan <janie177@hotmail.com>
2014-05-10 07:49:16 +0200

    GUI Command Added.

--- src/main/java/com/minegusta/commandgui/commands/GUICommand.java
+++ src/main/java/com/minegusta/commandgui/commands/GUICommand.java
@@ -1,15 +1,27 @@
 package com.minegusta.commandgui.commands;
 
+import com.minegusta.commandgui.GUICreator;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 
 public class GUICommand implements CommandExecutor
 {
 
 	@Override
-	public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings)
+	public boolean onCommand(CommandSender s, Command cmd, String label, String[] args)
 	{
-		return false;
+		if(!(cmd.getName().equalsIgnoreCase("gui")))return false;
+		if(s instanceof ConsoleCommandSender)return false;
+		else
+		{
+			Player p = (Player) s;
+			GUICreator.createGUI(p);
+			return true;
+		}
+
 	}
 }
