3be2973b447517fccf4d3e3e3f0e4e322cb7d4e4
Jan <janie177@hotmail.com>
2014-05-10 07:39:29 +0200

    Added opening of the GUI using maps.

--- src/main/java/com/minegusta/commandgui/listeners/GUIListener.java
+++ src/main/java/com/minegusta/commandgui/listeners/GUIListener.java
@@ -1,8 +1,15 @@
 package com.minegusta.commandgui.listeners;
 
+import com.minegusta.commandgui.GUICreator;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class GUIListener implements Listener
 {
@@ -27,6 +34,20 @@ public class GUIListener implements Listener
 			inv.closeInv();
 
 			inv.getPlayer().chat("/" + inv.getCommand());
+		}
+	}
+
+	@EventHandler
+	public void onRightClick(PlayerInteractEvent e)
+	{
+		Player p = e.getPlayer();
+		ItemStack s = p.getItemInHand();
+		Material m = s.getType();
+
+		if(!(m.equals(Material.EMPTY_MAP) && s.getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "Command Helper")))return;
+		if(e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
+		{
+			GUICreator.createGUI(p);
 		}
 	}
 }
